94546d391b0d8283ab89f3ea5dbfaae5cf7b62cf
m1k50118 <ac.backfill1539@gmail.com>
2022-12-31 13:58:54 +0900

    SwiftUI essentials|Using stacks to arrange views

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by 佐藤真 on 2022/12/31.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
